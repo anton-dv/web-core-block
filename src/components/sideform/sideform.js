eb5a78cab90ed16b15b9e0cb4f02183dd578ecf1
Anton Dvoininov <antonydvoininow@outlook.com>
2024-11-02 16:16:34 +0300

    to block-1.3

--- src/components/sideform/sideform.js
+++
@@ -1,9 +0,0 @@
-import { SideItem } from '../../app/js/SideItem.js';
-import { SideForm } from '../../app/js/SideFrom.js';
-
-const sideForm = new SideForm()
-
-new SideItem('#sideform', '#sideform-repair-opener', '#sidebform-closer', sideForm);
-new SideItem('#sideform', '#sideform-application-opener', '#sidebform-closer', sideForm);
-
-
